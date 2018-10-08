4a13aadd39b27f666dd84945a62d960f73076300
songzxDev <songzx_2326@163.com>
2018-10-08 21:09:51 +0800

    新建models文件夹

---
+++ webtreasures/models/__init__.py
@@ -0,0 +1,1 @@
+from webtreasures.models.fortunedrp import *
---
+++ webtreasures/models/fortunedrp.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/10/8 0008 下午 9:08
+# @Author  : songzxDev
+# @Email   : songzx_2326@163.com
+# @File    : fortunedrp.py
+# @Software: PyCharm
+
